1decd1b21b858459caf0ac06642396636c240fcc
Daniel <daniel.cabane@gmail.com>
2020-11-14 11:33:35 +0800

    youpi

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -7,8 +7,8 @@ const fs = require("fs");
         // eslint-disable-next-line no-console
         console.log("Building started...");
         await execa("npm", ["run", "build"]);
-        // Understand if it's dist or build folder
-        const folderName = fs.existsSync("dist") ? "dist" : "build";
+        // Understand if it's docs or build folder
+        const folderName = fs.existsSync("docs") ? "docs" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
