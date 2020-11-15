6b852646f2cc71f88c778747ed938534bc519c04
Daniel <daniel.cabane@gmail.com>
2020-11-15 22:04:31 +0800

    Etaler et multiplier

--- src/components/mainDeck.vue
+++ src/components/mainDeck.vue
@@ -3,9 +3,43 @@
     <v-row class="text-center">
       <v-col cols="12">
           <v-card class='pa-3'>
-            <v-btn fab icon small elevation='2' color='error' style='position:absolute;right:2px;top:2px' @click='deleteDialog = true'>
-              <v-icon>{{ svgPath }}</v-icon>
-            </v-btn>
+            <v-menu bottom left>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on" style='position:absolute;right:2px;top:2px;z-index:5'>
+                  <v-icon>{{ svgPathDots }}</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click='flatten' :disabled='totalTiles > 12'>
+                  <v-list-item-avatar><v-icon :color='totalTiles > 12 ? "grey lighten-1" : "black"'>{{ svgPathFlat }}</v-icon></v-list-item-avatar>
+                  <v-list-item-content>Etaler</v-list-item-content>
+                </v-list-item>
+                <v-list-item @click='multDialog = true' :disabled='!isFlat'>
+                  <v-list-item-avatar><v-icon :color='!isFlat ? "grey lighten-1" : "black"'>{{ svgPathMult }}</v-icon></v-list-item-avatar>
+                  <v-list-item-content>Multiplier</v-list-item-content>
+                </v-list-item>
+                <v-list-item @click='deleteDialog = true'>
+                  <v-list-item-avatar><v-icon>{{ svgPathDelete }}</v-icon></v-list-item-avatar>
+                  <v-list-item-content>Tout effacer</v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <v-dialog width='400px' v-model="multDialog">
+              <v-card>
+                <v-card-title>Multiplier par ?</v-card-title>
+                <v-card-actions style='display:block'>
+                  <v-form ref="multForm" v-model="multFormValid" lazy-validation>
+                    <div>
+                      <v-text-field autofocus v-model='multFactor' label="Multiplicateur" type='Number' min='1' max='10' step='1' outlined :rules='multRules'></v-text-field>
+                    </div>
+                  </v-form>
+                  <div style='display:flex;justify-content:space-around'>
+                    <v-btn width='150px' color='error' @click='multDialog = false'>Annuler</v-btn>
+                    <v-btn width='150px' color='primary' @click='multiply'>Multiplier</v-btn>
+                  </div>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
             <v-dialog width='400px' v-model="deleteDialog">
               <v-card>
                 <v-card-title>Effacer toutes les tuiles ?</v-card-title>
@@ -78,9 +112,9 @@
       </v-col>
       <v-col cols="12" style='display:flex'>
         <span v-for='(tileGroup, index) in tileGroups' :key='`group-${index}`' style='display:flex'>
-          <draggable group="tiles" @start="drag=true" @end="drag=false" :list="tileGroup"  @change='checkGroups'>
-            <singleTile v-for="tile in tileGroup" :key="tile.id" :h='tile.h' :v='tile.v' :id='tile.id'></singleTile>
-          </draggable>
+            <draggable group="tiles" @start="drag=true" @end="drag=false" :list="tileGroup"  @change='checkGroups'>
+              <singleTile v-for="tile in tileGroup" :key="tile.id" :h='tile.h' :v='tile.v' :id='tile.id'></singleTile>
+            </draggable>
           <span style='width:10px' v-if="margins[index]"></span>
         </span>
       </v-col>
@@ -91,17 +125,43 @@
 <script>
   import draggable from 'vuedraggable';
   import singleTile from './singleTile';
-  import { mdiDelete } from '@mdi/js';
+  import { mdiDelete, mdiDotsVertical, mdiCloseThick, mdiArrowLeftRightBold  } from '@mdi/js';
 
   export default {
     components: { draggable, singleTile },
     name: 'mainDeck',
     data: () => ({
-      svgPath: mdiDelete,
+      svgPathDelete: mdiDelete,
+      svgPathDots: mdiDotsVertical,
+      svgPathMult: mdiCloseThick,
+      svgPathFlat: mdiArrowLeftRightBold,
       deleteDialog: false,
+      multDialog: false,
+      multFactor: 1,
       tileGroups: [[], []],
-      margins: [true]
+      margins: [true],
+      multFormValid: true,
+      multRules: [
+        v => !!v || 'Saisir une valeur',
+        v => (v && v >=1 && v <= 10 && Math.floor(v)==v) || 'Valeur entière entre 1 et 10.',
+      ],
     }),
+    computed:{
+      totalTiles(){
+        let total = 0;
+        this.tileGroups.forEach(group => total += group.length);
+        return total;
+      },
+      isFlat(){
+        let flat = true;
+        this.tileGroups.forEach(group => {
+          if(group.length > 1){
+            flat = false;
+          } 
+        });
+        return flat;
+      }
+    },
     mounted(){
       this.eventBus.$on('deleteTile', id => {
          let tempArray = this.tileGroups;
@@ -180,12 +240,42 @@
         while(this.tileGroups[k].length == 0 && k > 0){
           k--;
         }
-        if(this.tileGroups[k][0] == undefined || this.tileGroups[k][0].h == newH){
+        if(this.tileGroups[k][0] == undefined || (this.tileGroups[k][0].h == newH && this.tileGroups[k][0].v == newV)){
           this.tileGroups[k].push({v: newV, h: newH, id: newId});
         } else {
           this.tileGroups.push([{v: newV, h: newH, id: newId}]);
         }
         this.checkGroups();
+      },
+      flatten(){
+        let allTiles = [];
+        this.tileGroups.forEach(group => {
+          group.forEach(tile => allTiles.push(tile));
+        });
+        this.tileGroups = [];
+        allTiles.forEach(tile => this.tileGroups.push([tile]));
+        this.checkGroups();
+      },
+      multiply(){
+        if(this.$refs.multForm.validate()){
+          let id = 1;
+          let allTiles = [];
+          this.tileGroups.forEach(group => {
+            group.forEach(tile => allTiles.push(tile));
+          });
+          this.tileGroups = [];
+          allTiles.forEach(tile => {
+            let group = [];
+            for(let k=0; k<this.multFactor; k++){
+              group.push({v:tile.v, h:tile.h, id});
+              id++;
+            }
+            this.tileGroups.push(group);
+          });
+          this.checkGroups();
+          this.$refs.multForm.reset();
+          this.multDialog = false;
+        }
       },
       clearAll(){
         this.tileGroups = [[],[]];
